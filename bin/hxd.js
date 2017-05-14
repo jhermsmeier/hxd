ef0ca5af15015d62797eec985998e71cdb541131
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-05-14 19:51:47 +0200

    feat(bin): Add -v, --version options

--- bin/hxd.js
+++ bin/hxd.js
@@ -3,6 +3,11 @@ var Hxd = require( '../lib/hxd' )
 var fs = require( 'fs' )
 var argv = process.argv.slice(2)
 
+if( argv.includes( '-v' ) || argv.includes( '--version' ) ) {
+  console.log( require( '../package.json' ).version )
+  process.exit( 0 )
+}
+
 var filename = argv.shift()
 var readStream = filename ?
   fs.createReadStream( filename ) :
