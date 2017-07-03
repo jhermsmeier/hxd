7c53f2c00c6363dfaf2406ab973b49f7e12f4377
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-07-03 22:51:10 +0200

    fix(bin): Fix malfunctioning argument handling

--- bin/hxd.js
+++ bin/hxd.js
@@ -3,25 +3,31 @@ var Hxd = require( '../lib/hxd' )
 var fs = require( 'fs' )
 var argv = process.argv.slice(2)
 
-if( argv.includes( '-v' ) || argv.includes( '--version' ) ) {
+var flags = argv.filter(( arg ) => {
+  return /^-/.test( arg )
+})
+
+if( flags.includes( '-v' ) || flags.includes( '--version' ) ) {
   console.log( require( '../package.json' ).version )
   process.exit( 0 )
 }
+
+var options = {
+  color: flags.includes( '--color' ) || process.stdout.isTTY
+}
+
+if( flags.includes( '--no-color' ) ) {
+  options.color = false
+}
+
+argv = argv.filter(( arg ) => {
+  return !/^-/.test( arg )
+})
 
 var filename = argv.shift()
 var readStream = filename ?
   fs.createReadStream( filename ) :
   process.stdin
-
-var options = {
-  color: process.stdout.isTTY
-}
-
-if( argv.includes( '--color' ) ) {
-  options.color = true
-} else if( argv.includes( '--no-color' ) ) {
-  options.color = false
-}
 
 readStream.pipe( new Hxd( options ) )
   .pipe( process.stdout )
